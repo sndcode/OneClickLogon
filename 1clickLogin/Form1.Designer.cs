a6a5e10f0a63691bbcc9badb2ea303cca7b856f8
Sandaasu <mailsandaasu@gmail.com>
2021-05-04 23:50:24 +0200

    1
    1

---
+++ 1clickLogin/Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace _1clickLogin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(147, 129);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(124, 48);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Login";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 129);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(121, 48);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Save";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 24);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(124, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(147, 25);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.PasswordChar = '*';
+            this.textBox2.Size = new System.Drawing.Size(124, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Username";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(144, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Password";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(277, 27);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(31, 17);
+            this.radioButton1.TabIndex = 6;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 50);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(124, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(147, 50);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.PasswordChar = '*';
+            this.textBox4.Size = new System.Drawing.Size(124, 20);
+            this.textBox4.TabIndex = 3;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(12, 76);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(124, 20);
+            this.textBox5.TabIndex = 2;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(147, 76);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.PasswordChar = '*';
+            this.textBox6.Size = new System.Drawing.Size(124, 20);
+            this.textBox6.TabIndex = 3;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(12, 103);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(124, 20);
+            this.textBox7.TabIndex = 2;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(147, 102);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.PasswordChar = '*';
+            this.textBox8.Size = new System.Drawing.Size(124, 20);
+            this.textBox8.TabIndex = 3;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(277, 50);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(31, 17);
+            this.radioButton2.TabIndex = 6;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(277, 77);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(31, 17);
+            this.radioButton3.TabIndex = 6;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "3";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(277, 102);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(31, 17);
+            this.radioButton4.TabIndex = 6;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "4";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 2500;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(318, 189);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "One Click Logon Revision B1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ 1clickLogin/Form1.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+using System.Linq;
+
+namespace _1clickLogin
+{
+    
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void m_SaveSettings()
+        {
+            try
+            {
+                if (File.Exists("C:\\users\\public\\oneclick.db"))
+                {
+                    var ini = new classINI("C:\\users\\public\\oneclick.db");
+                    ini.Write("username1", textBox1.Text);
+                    ini.Write("password1", classAtom128.Encode(textBox2.Text));
+                    ini.Write("username2", textBox3.Text);
+                    ini.Write("password2", classAtom128.Encode(textBox4.Text));
+                    ini.Write("username3", textBox5.Text);
+                    ini.Write("password3", classAtom128.Encode(textBox6.Text));
+                    ini.Write("username4", textBox7.Text);
+                    ini.Write("password4", classAtom128.Encode(textBox8.Text));
+                }
+                else
+                {
+                    File.Create("C:\\users\\public\\oneclick.db").Close();//Close file after spawning
+                    var ini = new classINI("C:\\users\\public\\oneclick.db");
+                    ini.Write("username1", textBox1.Text);
+                    ini.Write("password1", classAtom128.Encode(textBox2.Text));
+                    ini.Write("username2", textBox3.Text);
+                    ini.Write("password2", classAtom128.Encode(textBox4.Text));
+                    ini.Write("username3", textBox5.Text);
+                    ini.Write("password3", classAtom128.Encode(textBox6.Text));
+                    ini.Write("username4", textBox7.Text);
+                    ini.Write("password4", classAtom128.Encode(textBox8.Text));
+                }
+            }
+            catch
+            {
+                File.Create("C:\\users\\public\\oneclick.db").Close();//Close file after spawning
+            }
+        }
+        public void m_loadSettings()
+        {
+            try
+            {
+                if (File.Exists("C:\\users\\public\\oneclick.db"))
+                {
+                    var ini = new classINI("C:\\users\\public\\oneclick.db");
+                    textBox1.Text = ini.Read("username1");
+                    textBox2.Text = classAtom128.Decode(ini.Read("password1"));
+                    textBox3.Text = ini.Read("username2");
+                    textBox4.Text = classAtom128.Decode(ini.Read("password2"));
+                    textBox5.Text = ini.Read("username3");
+                    textBox6.Text = classAtom128.Decode(ini.Read("password3"));
+                    textBox7.Text = ini.Read("username4");
+                    textBox8.Text = classAtom128.Decode(ini.Read("password4"));
+
+                }
+                else
+                {
+                    File.Create("C:\\users\\public\\oneclick.db").Close();//Close file after spawning
+                    var ini = new classINI("C:\\users\\public\\oneclick.db");
+                    ini.Write("username1", textBox1.Text);
+                    ini.Write("password1", classAtom128.Encode(textBox2.Text));
+                    ini.Write("username2", textBox3.Text);
+                    ini.Write("password2", classAtom128.Encode(textBox4.Text));
+                    ini.Write("username3", textBox5.Text);
+                    ini.Write("password3", classAtom128.Encode(textBox6.Text));
+                    ini.Write("username4", textBox7.Text);
+                    ini.Write("password4", classAtom128.Encode(textBox8.Text));
+                }
+            }
+            catch
+            {
+                File.Create("C:\\users\\public\\oneclick.db").Close();//Close file after spawning
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            m_SaveSettings();
+        }
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            MessageBox.Show("Website will launch in default broser now");
+            System.Diagnostics.Process.Start("http://");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int i_accselected = 0;
+            if(radioButton1.Checked == true)
+            {
+                i_accselected = 1;
+            }
+            else if (radioButton2.Checked == true)
+            {
+                i_accselected = 2;
+            }
+            else if (radioButton3.Checked == true)
+            {
+                i_accselected = 3;
+            }
+            else if (radioButton4.Checked == true)
+            {
+                i_accselected = 4;
+            }
+
+
+            try
+            {
+                if (File.Exists("C:\\users\\public\\oneclick.db") && i_accselected != 0)
+                {
+                    //Retrieve saved ones
+                    //Timer waiter = new Timer();
+                    //waiter.Interval = 2500;
+                    //waiter.Start();
+                    System.Threading.Thread.Sleep(2100);
+                    //Free some ram
+                    //Do Login
+                    // Find The Window Handle For VPN
+                    //IntPtr handle = FindWindow("Pulse", "PulseApplicationLauncher");
+                    //if (!handle.Equals(IntPtr.Zero))
+                    //{
+                    // Activate The VPN Window
+                    //if (SetForegroundWindow(handle))
+
+                    //var proc = Process.GetProcessesByName("Verbinden mit:SAPROV").FirstOrDefault();
+                    //if (proc != null && proc.MainWindowHandle != IntPtr.Zero)
+                    //{
+                    //SetForegroundWindow(proc.MainWindowHandle);
+
+                    string selectedUser = "";
+                    string selectedPass = "";
+                    
+                    if(i_accselected == 1)
+                    {
+                        selectedUser = textBox1.Text;
+                        selectedPass = textBox2.Text;
+                    }
+                    else if (i_accselected == 2)
+                    {
+                        selectedUser = textBox3.Text;
+                        selectedPass = textBox4.Text;
+                    }
+                    else if (i_accselected == 3)
+                    {
+                        selectedUser = textBox5.Text;
+                        selectedPass = textBox6.Text;
+                    }
+                    else if (i_accselected == 4)
+                    {
+                        selectedUser = textBox7.Text;
+                        selectedPass = textBox8.Text;
+                    }
+
+                    SendKeys.Send(selectedUser);
+                    System.Threading.Thread.Sleep(500);//Free some ram
+                    SendKeys.Send("{TAB}");
+                    System.Threading.Thread.Sleep(500);//Free some ram
+                    SendKeys.Send(selectedPass);
+                    System.Threading.Thread.Sleep(500);//Free some ram
+                    SendKeys.Send("{ENTER}");
+                }
+                else
+                {
+                    File.Create("C:\\users\\public\\oneclick.db").Close();//Close file after spawning
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Error while logging in. Make sure you entered logins and clicked a button which account to use.");
+            }
+        }
+
+        private bool SetForegroundWindow(IntPtr handle)
+        {
+            throw new NotImplementedException();
+        }
+
+        private IntPtr FindWindow(string v1, string v2)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (File.Exists("C:\\users\\public\\oneclick.db"))
+            {
+                m_loadSettings();
+            }
+            else
+            {
+                MessageBox.Show("no settings found, please enter login data and click save.");
+            }
+        }
+    }
+}
